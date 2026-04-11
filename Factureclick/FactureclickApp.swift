9cf72006e485c21bc2239afe4d53b9e11e6d0da6
Macdirtycow <info@quietsync.net>
2026-04-11 23:59:02 +0200

    Initial Commit

---
+++ Factureclick/FactureclickApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactureclickApp.swift
+//  Factureclick
+//
+//  Created by Leopold on 11/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FactureclickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
